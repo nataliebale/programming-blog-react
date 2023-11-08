e192b1773d4bbb5dcde20c0b26b7c22666f127cc
nataliabalakhashvili <natalia.balakhashvili@geolab.edu.ge>
2023-11-09 00:48:12 +0400

    feat - clearing search text functionality / not finished

--- src/components/blog/Search.js
+++ src/components/blog/Search.js
@@ -1,15 +1,31 @@
 import '../../styles/search.css';
+import deleteIcon from '../../assets/images/x-solid.svg';
 import searchIcon from '../../assets/images/magnifying-glass-solid.svg';
+import { useState } from "react";
 
 function Search({ handleChange }){
+  const [searchValue, setSearchValue] = useState('');
+
+  const onInputChange = (event) => {
+    setSearchValue(event);
+    handleChange(event)
+  }
+
+  const onClearSearchField = () => {
+    setSearchValue('');
+  }
+
   return (
     <div className='search-block'>
-      <img className='search-icon' src={searchIcon} alt=""/>
+      { searchValue ?
+        (<img className='search-icon' src={deleteIcon} alt=""/>) :
+        (<img className='search-icon' onClick={() => onClearSearchField()} src={searchIcon} alt=""/>)
+      }
       <input
         className='search-input'
         type="text"
         placeholder='Search posts'
-        onChange={(e) => handleChange(e.target.value)}
+        onChange={(e) => onInputChange(e.target.value)}
       />
     </div>
   )
